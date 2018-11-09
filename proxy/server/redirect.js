deb2197a2060df0ffc84d29f60cb38b2d4e86e43
lfelberg <lfelberg@berkeley.edu>
2018-11-09 15:37:13 -0800

    Adding in glory's updated AWS Server

--- proxy/server/redirect.js
+++ proxy/server/redirect.js
@@ -1,8 +1,7 @@
 const httpProxy = require('http-proxy');
 
 const apiProxy = httpProxy.createProxyServer();
-//const serverUser = `${process.env.USER_SERVER}`;
-const serverUser = `http://localhost:${process.env.USER_PORT}`;
+const serverUser = `${process.env.USER_SERVER}`;
 const ServerRelated = `${process.env.RELATED_SERVER}`;
 const ServerComment = `${process.env.COMMENTS_SERVER}`;
 const ServerPlayer = `${process.env.PLAYER_SERVER}`;
